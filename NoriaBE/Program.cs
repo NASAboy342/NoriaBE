23695dc4fa8db9205571da571128049bb47175aa
PinSopheaktra <sopheakpin342@gmail.com>
2026-05-09 18:29:49 +0700

    allow any rigin

--- NoriaBE/Program.cs
+++ NoriaBE/Program.cs
@@ -15,6 +15,15 @@ builder.Services.AddSingleton<IBuildingService, BuildingService>();
 builder.Services.AddSingleton<IRoomService, RoomService>();
 builder.Services.AddSingleton<INoriaRepository, NoriaRepository>();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -24,6 +33,7 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseCors("AllowLocalhost");
 
 app.UseHttpsRedirection();
 
